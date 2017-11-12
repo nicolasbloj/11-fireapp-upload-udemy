126daea42332f1291a665f2e6f847a130c13066f
Nicolas Bloj <nicolasbloj@gmail.com>
2017-11-12 03:52:58 -0300

    validations into dropzone directive

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -28,6 +28,65 @@ export class NgDropFilesDirective {
 
   @HostListener('dragover', ['$event'])
   public onDragOver(event: any) {
+    const transfer: any = this._getTransfer(event);
+    transfer.dropEffect = 'copy';
+    this._preventAndStop(event);
     this.fileOver.emit(true);
+  }
+
+  @HostListener('drop', ['$event'])
+  public onDrop(event: any) {
+    const transfer: any = this._getTransfer(event);
+    if (!transfer) {
+      return;
+    }
+    this._addFiles(transfer.files);
+    this._preventAndStop(event);
+  }
+
+  // necesitamos controlar si existe algo a transferir
+  private _getTransfer(event: any): boolean {
+    // console.log('_getTransfer - event ', event);
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  // stop propagation!
+  // necesitamos prevenir el comportamiento por defecto
+  private _preventAndStop(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  // determinar si el archivo ya fue dropeado para agregar a array this.files
+  // Archivo Ya Fue Dropeado
+  private _fileWasDropedYet(fileName: string): boolean {
+
+    // tslint:disable-next-line:forin
+    for (const i in this.files) {
+      const file = this.files[i];
+      if (file.name === fileName) {
+        console.log('Archivo ya existe en la lista', fileName);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Validamos que sea imagen el archivo
+  private _fileIsImage(fileType: string): boolean {
+    return (fileType === '' || fileType === undefined) ? false : fileType.startsWith('image');
+  }
+
+  // resumimos todas las validaciones
+  private _fileCanSave(file: File) {
+    if (!this._fileWasDropedYet(file.name) && this._fileIsImage(file.type)) {
+      return true;
+    }
+    return false;
+  }
+
+
+  private _addFiles(fileList: FileList) {
+    console.log('filelist ', fileList);
   }
 }
